4c133ff8214d1b8bd93e2a52150b655ef4a26589
shaimaa <shaimafikry@yahoo.com>
2024-08-11 12:13:27 +0300

    add 4

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -3,3 +3,16 @@
 -- Quantity in the table items can be negative.
 
 -- Context: Updating multiple tables for one action from your application can generate issue: network disconnection, crash, etc… to keep your data in a good shape, let MySQL do it for you!
+
+DELIMITER //  -- i change the end delimeter so that i can do more than one statment without ending the block
+CREATE TRIGGER sum_quantity -- creation and name
+AFTER  -- trigger time
+INSERT -- trigger event 
+ON orders  -- table that activate trigger
+FOR EACH ROW 
+BEGIN
+  UPDATE items
+  set quantity = quantity - NEW.number -- NEW represent the new value
+  WHERE name = NEW.item_name;
+END //
+DELIMITER ; -- i retrieve the normal delimiter
