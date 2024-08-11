d19f99324dd187eb318cdcac9e2c1867f66569d7
shaimaa <shaimafikry@yahoo.com>
2024-08-11 12:45:48 +0300

    add 4

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -4,15 +4,13 @@
 
 DELIMITER // -- DELIMTER CHANGE
 CREATE TRIGGER email_reset
-AFTER --time
-UPDATE --event
-ON users -- trigger table
--- trigger body
-FOR EACH ROW
-BEGIN
-  IF NEW.email != OLD.email
+AFTER UPDATE ON users  -- event time trigger table
+FOR EACH ROW -- EXCUTE ON EVERY ROW
+BEGIN -- trigger body
+  IF NEW.email != OLD.email THEN
     UPDATE users
-    SET valid_email = 1 WHERE user_id = NEW.user_id;
+    SET valid_email = 1
+    WHERE id = NEW.id;
   END IF;
 END //
 DELIMITER ; -- DELEIMTER RETRIVE
