d5c37cd50cd93628af58450b5464309a75091c3b
shaimaa <shaimafikry@yahoo.com>
2024-08-11 05:47:28 +0300

    add 2

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -4,7 +4,16 @@
 
 -- Import this table dump: metal_bands.sql.zip
 -- Column names must be: band_name and lifespan (in years until 2022 - please use 2022 instead of YEAR(CURDATE()))
+-- last year to calculate from is 2022
 -- You should use attributes formed and split for computing the lifespan
 -- Your script can be executed on any database
+-- The CASE statement calculates the lifespan. If the split year is less than or equal to 2022, it uses split - formed. Otherwise, it uses 2022 - formed to handle bands that have not split yet or have split years beyond 2022.
 
-SELECT band_name, SUM(split - formed) AS lifespan FROM metal_bands WHERE style = 'Glam rock' GROUP BY band_name;
+SELECT band_name,
+CASE
+WHEN split <= 2022 THEN split - formed
+ELSE 2022 - formed
+END AS lifespan
+FROM metal_bands
+WHERE style = 'Glam rock'
+ORDER BY lifespan;
