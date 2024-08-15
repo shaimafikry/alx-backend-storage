5c2568f99525e9a3ec9be45607ee71776ba676c2
shaimaa <shaimafikry@yahoo.com>
2024-08-15 06:32:19 +0300

    add new task

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,8 +1,28 @@
 #!/usr/bin/env python3
 """ module for redis task"""
 import redis
-from typing import Any, Callable, Optional, Union
+from typing import Any, Callable, Optional, Union, T
 import uuid
+from functools import wraps
+
+
+# Above Cache define a count_calls decorator that takes
+# a single method Callable argument and returns a Callable.
+def count_calls(fn: Callable) -> Callable:
+    """ count calls"""
+    @wraps(fn)
+    # this func gonna create anew key wth the method name
+    # and increse its value every time it's called
+    def wrapper(self, *arg, **kwargs):
+        """ the wrraper func"""
+        # attribute provides the qualified name of a method,
+        # including the class it belongs to. For example, for
+        # amethod foo in class Bar, foo.__qualname__ would
+        # return'Bar.foo'.
+        key = fn.__qualname__
+        self._redis.incr(key)
+        return fn(self, *arg, **kwargs)
+    return wrapper
 
 
 class Cache:
@@ -11,13 +31,15 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[float, str, int, bytes]) -> str:
         """ store key """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: Optional[Callable]) -> Any:
+    def get(self, key: str,
+            fn: Optional[Callable[[Any], Any]] = None) -> Optional[Any]:
         """ convert data"""
         value = self._redis.get(key)
         if value is None:
