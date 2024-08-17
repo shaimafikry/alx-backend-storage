adbe02448cbb0074e0a0bac38782c7bf4a9e6ab9
shaimaa <shaimafikry@yahoo.com>
2024-08-17 05:25:36 +0300

    add files

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,1 +1,37 @@
 #!/usr/bin/env python3
+"""get page func"""
+import requests
+import redis
+from typing import Callable
+from functools import wraps
+
+
+con = redis.Redis()
+
+
+def count_get(method: Callable) -> str:
+    """count how many method called"""
+    @wraps(method)
+    def wrapper(url):
+        req = method(url)
+        key = f"count:{url}"
+        cashe_key = f"cache:{url}"
+        con.set(cashe_key, 10, req)
+        con.incr(key)
+        return req
+    return wrapper
+
+
+@count_get
+def get_page(url: str) -> str:
+    """It uses the requests module to obtain the HTML
+    content of a particular URL and returns it."""
+    data = requests.get(url)
+    return data.text
+
+# url = "http://slowwly.robertomurray.co.uk"
+# page_content = get_page(url)
+# print(f"Page content: {len(page_content)}")
+
+# # Print the count of accesses
+# print(f"Access count: {con.get(f'count:{url}').decode('utf-8')}")
