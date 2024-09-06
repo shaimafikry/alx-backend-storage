602613b2b9ff37e36df1533898f9ef10fd385f7e
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2024-09-06 21:29:20 +0300

    Update 102-log_stats.py

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -31,9 +31,22 @@ def main():
     for k, v in method.items():
         print(f"	method {k}: {v}")
     print(collection.count_documents({'path': '/status'}), "status check")
+
+    ips = {
+      "172.31.63.67": collection.count_documents({'ip': '172.31.63.67'}),
+           "172.31.2.14": collection.count_documents({'ip': '172.31.2.14'}),
+           "172.31.29.194": collection.count_documents({'ip': '172.31.29.194'}),
+           "69.162.124.230": collection.count_documents({'ip': '69.162.124.230'}),
+           "64.124.26.109": collection.count_documents({'ip': '64.124.26.109'}),
+           "64.62.224.29": collection.count_documents({'ip': '64.62.224.29'}),
+           "34.207.121.61": collection.count_documents({'ip': '34.207.121.61'}),
+           "47.88.100.4": collection.count_documents({'ip': '47.88.100.4'}),
+           "45.249.84.250": collection.count_documents({'ip': '45.249.84.250'}),
+           "216.244.66.228": collection.count_documents({'ip': '216.244.66.228'})
+           }
     print("IPs:")
-    for i in data:
-        print(f"	{i['ip']}")
+    for k, v in ips.items():
+        print(f"	{k}: {v}")
 
     # 94778 logs
     # Methods:
