9659a16185c65feda712abbb284d62e7d4a7ee96
shaimaa <shaimafikry@yahoo.com>
2024-09-06 15:48:54 +0300

    add 12

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""Python script that provides some stats about Nginx logs stored in MongoDB:
+ """
+from pymongo import MongoClient
+# connect the data base
+client = MongoClient()
+# access data base
+db = client.logs
+# access collection
+collection = db.nginx
+
+# count
+count_data = collection.count_documents({})
+print (f"{count_data} logs")
+data = collection.find()
+# print(list(data))
+get_method = collection.count_documents({'method': 'GET'})
+post_method = collection.count_documents({'method': 'POST'})
+put_method = collection.count_documents({'method': 'PUT'})
+patch_method = collection.count_documents({'method': 'PATCH'})
+delete_method = collection.count_documents({'method': 'DELETE'})
+method = {"GET": get_method, "POST": post_method, "PUT": put_method,
+          "PATCH": patch_method, "DELETE": delete_method}
+
+# print(data_method)
+print("Methods:")
+for k, v in method.items():
+    print(f"	Method {k}: {v}")
+print(collection.count_documents({'path': '/status'}), "status check")
+# 94778 logs
+# Methods:
+#     method GET: 93842
+#     method POST: 229
+#     method PUT: 0
+#     method PATCH: 0
+#     method DELETE: 0
+# 47415 status check
